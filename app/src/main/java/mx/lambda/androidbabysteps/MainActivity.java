97aa36476a0a72c33c29ae4d2a4ddfb1c169ef68
Abimael Luis <abi@airmail.cc>
2016-06-27 18:35:24 -0500

    First two Activities c:

---
+++ app/src/main/java/mx/lambda/androidbabysteps/MainActivity.java
@@ -0,0 +1,26 @@
+package mx.lambda.androidbabysteps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "mx.lambda.androidbabysteps.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.inputText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
